110989b4afeecbd1074fd61a63448a4850d23d3c
BillyQin <965824981@qq.com>
2019-04-14 11:37:31 +0800

    add mode

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Logo from './components/logo';
 import { generateColor, generateIcon } from './util/common';
 
+const MODE = [1,2,3,4]
+
 class App extends Component {
   constructor() {
     super(...arguments);
     this.state = {
       background: 0,
       icon: 0,
-      mode: 1
+      mode: 1,
+      text: ''
     }
+    this.titleText = ''
   }
 
   componentDidMount() {
@@ -34,37 +38,71 @@ class App extends Component {
     })
   }
 
-  changeMode = () => {
+  changeMode = (mode) => {
     this.setState({
-      mode: this.state.mode===1?2:1
+      mode
     })
+  }
+
+  changeText = () => {
+    this.setState({
+      text: this.titleText
+    })
+  }
+
+  changeTextHandle = ({target}) => {
+    this.titleText = target.value
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <div className="button-group">
-            <button onClick={this.changeColor}>
-              背景色
-            </button>
-            <button onClick={this.changeMode}>
-              模式
-            </button>
-            <button onClick={this.changeIcon}>
-              图案
-            </button>
-            {/* <button onClick={this.changeFont}>
-              字体
-            </button> */}
+        <div className="button-group">
+          <div className="form-item">
+            <h3>文本</h3>
+            <div className="item">
+              <input onChange={this.changeTextHandle}/>
+              <button onClick={this.changeText}>确定</button>
+            </div>
           </div>
-          
-          <Logo 
+          <div className="form-item">
+            <h3>背景色</h3>
+            <div className="item">
+              <input />
+              <button onClick={this.changeColor}>确定</button>
+            </div>
+            <div className="item">
+              <span>{this.state.background}</span>
+              <button onClick={this.changeColor}>随机生成</button>
+            </div>
+          </div>
+          <div className="form-item">
+            <h3>图案</h3>
+            <div className="item">
+              <span>图案{this.state.icon}</span>
+              <button onClick={this.changeIcon}>随机生成</button>
+            </div>
+            <div className="item">
+              <button onClick={this.changeColor}>图案列表</button>
+            </div>
+          </div>
+          <div className="form-item">
+            <h3>模式</h3>
+            <div className="button-group">
+              {MODE.map((item,key) => (
+                <button key={key} onClick={()=>this.changeMode(item)}>模式{item}</button>
+              ))}
+            </div>
+          </div>
+        </div>
+        <main>
+          <Logo
             background={this.state.background}
             icon={this.state.icon}
             mode={this.state.mode}
+            text={this.state.text}
           />
-        </header>
+        </main>
       </div>
     );
   }
--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -5,8 +5,10 @@ class Logo extends PureComponent {
   constructor() {
     super(...arguments);
     this.state = {
-      icon: 0,
-      mode: 1
+      icon: null,
+      mode: 1,
+      background: '',
+      text: ''
     }
     this.mode = {}
   }
@@ -20,46 +22,87 @@ class Logo extends PureComponent {
   }
 
   static getDerivedStateFromProps(nextProps) {
-    const { icon,mode } = nextProps
+    const { icon, mode, background, text } = nextProps
     return {
       icon,
-      mode
+      mode,
+      background,
+      text
     }
   }
 
   /**
    * 绘画logo
    * @param {String} text 文字
-   * @param {Number} mode [1,2]
+   * @param {Number} mode [1,2,3,4]
    * @param {Number} upper 上限
    * @return {Number} 返回在下限到上限之间的一个随机整数
    */
-  drawLogo = (text='无印良品') => {
-    const { icon, mode } = this.state
+  drawLogo = () => {
+    const { icon, mode, background, text = '无印良品' } = this.state
     let c = this.refs.canvas;
     let ctx = c.getContext("2d");
-    ctx.clearRect(0,0,1000,1000);
-    // 底部半圆2
-    ctx.beginPath()
-    ctx.arc(200, 760, 460, 0, Math.PI, true);
-    ctx.fillStyle = mode===1?'rgb(255,255,255)':'rgb(249,205,173)';
-    ctx.strokeStyle = "rgb(255,255,255)";
+    ctx.clearRect(0, 0, 1000, 1000);
+
+    // 背景色
+    ctx.beginPath();
+    ctx.rect(0, 0, 1000, 1000);
+    ctx.fillStyle = background;
     ctx.fill();
     ctx.closePath();
 
-    // 底部半圆1
-    ctx.beginPath()
-    ctx.arc(200, 620, 320, 0, Math.PI, true);
-    ctx.fillStyle = 'rgb(255,255,255)';
-    ctx.strokeStyle = "rgb(255,255,255)";
-    ctx.fill();
-    ctx.closePath();
+    if (mode === 4) {
+      // 波浪线
+      // 贝塞尔曲线
+      // http://blogs.sitepointstatic.com/examples/tech/canvas-curves/bezier-curve.html 
+      ctx.beginPath();
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = "#333";
+      ctx.stroke();
+      // 起始点
+      ctx.moveTo(0, 282);
+      // 控制点1(x,y),控制点2(x,y),终点
+      // cpx1,cpy1,cpx2,cpy2, endx, endy
+      ctx.bezierCurveTo(163, 406, 246, 220, 400, 308);
+      ctx.lineTo(400, 400);
+      ctx.lineTo(0, 400);
+      ctx.fillStyle = '#fff';
+      ctx.fill();
+      ctx.closePath();
+    } else {
+      // 底部大半圆
+      ctx.beginPath()
+      ctx.arc(200, 760, 460, 0, Math.PI, true);
+      ctx.fillStyle = mode === 1 ? 'rgb(255,255,255)' : 'rgb(249,205,173)';
+      switch (mode) {
+        case 1: ctx.fillStyle = 'rgb(255,255,255)'; break;
+        case 2: ctx.fillStyle = 'rgb(249,205,173)'; break;
+        case 3: ctx.fillStyle = background; break;
+        default: break;
+      }
+      ctx.strokeStyle = "rgb(255,255,255)";
+      ctx.fill();
+      ctx.closePath();
+
+      // 底部小半圆
+      ctx.beginPath()
+      ctx.arc(200, 620, 320, 0, Math.PI, true);
+      switch (mode) {
+        case 1:
+        case 2: ctx.fillStyle = 'rgb(255,255,255)'; break;
+        case 3: ctx.fillStyle = background; break;
+        default: break;
+      }
+      ctx.strokeStyle = "rgb(255,255,255)";
+      ctx.fill();
+      ctx.closePath();
+    }
 
     // 底部字体
     ctx.beginPath()
     ctx.fillStyle = 'rgb(25,25,25)';
     ctx.font = "bold 30px arial";
-    ctx.textAlign="center";
+    ctx.textAlign = "center";
     ctx.fillText(text, 200, 360);
     ctx.stroke();
     ctx.closePath();
@@ -67,34 +110,32 @@ class Logo extends PureComponent {
     let img = new Image()
     img.src = require(`../../img/${icon}.svg`);
     img.onload = () => {
-      ctx.drawImage(img, 100, 50, 200,200);
+      ctx.drawImage(img, 100, 50, 200, 200);
     }
   }
 
   downLoad = () => {
-    // let image = new Image();
     let c = this.refs.canvas;
-    // image.src = c.toDataURL("image/png");
-    var a = document.createElement('a');
+    let a = document.createElement('a');
     var event = new MouseEvent('click');
+    a.download = 'logo';
     a.href = c.toDataURL("image/png");
-    a.dispatchEvent(event) 
-    return null
+    a.dispatchEvent(event)
   }
 
   render() {
-    const { background } = this.props;
     return (
-      <>
-        <canvas 
-          width={400} 
-          height={400} 
-          className="logo" 
+      <main>
+        <canvas
+          width={400}
+          height={400}
+          className="logo"
           ref='canvas'
-          style={{background}}
         ></canvas>
-        <button onClick={this.downLoad} style={{marginTop: '20px'}}>下载png</button>
-      </>
+        <div className="down-load">
+          <button onClick={this.downLoad} style={{ marginTop: '20px' }}>下载png</button>
+        </div>
+      </main>
     )
   }
 }
